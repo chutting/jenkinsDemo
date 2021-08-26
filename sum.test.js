3cd3531d4555d826050094879ee16a43643528b4
Chu Tingting <tingting.chu@thoughtworks.com>
2021-08-26 14:51:41 +0800

    test automatic build

--- sum.test.js
+++ sum.test.js
@@ -2,5 +2,5 @@ const { test, expect } = require('@jest/globals')
 const sum = require('./sum')
 
 test('expect 1 plus 2 equals 3', () => {
-  expect(sum(4, 2)).toBe(6)
+  expect(sum(4, 3)).toBe(8)
 })
